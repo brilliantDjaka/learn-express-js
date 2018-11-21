c40aa0b909e3f307e069b27b2a50e18f19047f47
Brian Rofiq <brian.rofiq@gmail.com>
2018-11-21 09:22:50 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const os = require('os')
+const hbs = require('hbs')
+const fs = require('fs')
+
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('getYear',()=>new Date().getFullYear())
+hbs.registerHelper('UpperIt',(text)=>text.toUpperCase())
+
+
+app.use((req,res,next)=>{
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`
+   
+    fs.appendFile('serverLog.txt',log + '\n',err => err?console.log(err):null);
+    console.log(log);
+    
+    
+    next();
+})
+app.use((req,res,next)=>{
+    res.render('maintance.hbs',{
+        pageTitle:'Maintance'
+    })
+})
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+      pageTitle:'Home',
+      welcomeMessage:'Haloooo'
+  })
+})
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About'
+    })
+})
+app.get('/bad',(req,res)=>{
+    res.send('unable to handle request')
+})
+
+
+app.listen(9000,()=>console.log('Server is running on port 9000'))
